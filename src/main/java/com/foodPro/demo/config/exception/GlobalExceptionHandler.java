11035d95acaa909d7b35c825f4c164ab745070c1
HyeonWuJeon <tkawnd111@gmail.com>
2020-11-15 17:56:50 +0900

    Controller 전역 오류 / 중복 테스트

--- src/main/java/com/foodPro/demo/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/foodPro/demo/config/exception/GlobalExceptionHandler.java
@@ -3,51 +3,88 @@ package com.foodPro.demo.config.exception;
 import com.foodPro.demo.config.common.ApplicationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 
-import javax.validation.ConstraintViolationException;
-import java.util.HashMap;
+import java.util.Date;
 
-@RestControllerAdvice
-public class GlobalExceptionHandler extends ApplicationService {
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler extends ApplicationService{
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+//    /**
+//     * FUNCTION :: Checked 오류
+//     * @param
+//     */
+//    @ExceptionHandler(Exception.class)
+//    public void globalException(Exception e) {
+//        log.info("Exception [" + e.getClass() + "] [" + e.getMessage() + "]");
+//        HashMap<String, Object> rtnMap = returnMap();
+//        rtnMap.put(AJAX_RESULT_TEXT, AJAX_RESULT_FAIL);
+//    }
+
     /**
-     * FUNCTION :: Checked 오류
-     * @param e
+     * Handler :: UserNotFoundException Error :: 정보 조회 오류
+     * @param ex
+     * @param request
+     * @return
      */
-    @ExceptionHandler(Exception.class)
-    public void globalException(Exception e) {
-        log.info("Exception [" + e.getClass() + "] [" + e.getMessage() + "]");
-        HashMap<String, Object> rtnMap = returnMap();
-        rtnMap.put(AJAX_RESULT_TEXT, AJAX_RESULT_FAIL);
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleAllExceptions(UserNotFoundException ex, WebRequest request) {
+        log.info("[" + ex.getClass() + "] [" + ex.getMessage() + "]");
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     /**
-     * FUNCTION :: Validation 오류
+     * Handler :: Validation Error :: 회원가입 입력값 검증 오류 --> resultBinding 해결
+     * @param ex
+     * @param request
+     * @return
      */
-    @ExceptionHandler(ConstraintViolationException.class)
-    public String ValidationException(ConstraintViolationException e) {
-        log.info("Exception Validation [" + e.getClass() + "] [" + e.getMessage() + "]");
-        HashMap<String, Object> rtnMap = returnMap();
-        rtnMap.put(AJAX_RESULT_TEXT, AJAX_RESULT_ILLEGAL_STATE); //LINE :: 유효성 검증 오류
-        return jsonFormatTransfer(rtnMap);
-    }
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    public ResponseEntity<Object> ValidationException(ConstraintViolationException ex, WebRequest request) {
+//        log.info("Exception Validation [" + ex.getClass() + "] [" + ex.getMessage() + "]");
+//        HashMap<String, Object> rtnMap = returnMap();
+//        ExceptionResponse exceptionResponse =
+//                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity(exceptionResponse, HttpStatus.UNAUTHORIZED);
+//    }
+
 
     /**
-     * FUNCTION :: Duplication 오류
-     * @param e
+     * Handler :: MemberDuplicationException :: 회원가입 이메일 중복 오류
+     * @param ex
      * @return
      */
     @ExceptionHandler(MemberDuplicationException.class)
-    public String DuplicationException(MemberDuplicationException e) {
-        log.info("Exception Validation [" + e.getClass() + "] [" + e.getMessage() + "]");
-        HashMap<String, Object> rtnMap = returnMap();
-        rtnMap.put(AJAX_RESULT_TEXT, AJAX_RESULT_DUPLICATE); //LINE :: 이메일 중복 오류
-        return jsonFormatTransfer(rtnMap);
+    public ResponseEntity<Object> DuplicationException(MemberDuplicationException ex, WebRequest request) {
+        log.info("Exception Duplication [" + ex.getClass() + "] [" + ex.getMessage() + "]");
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST); //Body에 예외내용 담김.
+
+//        return new ResponseEntity(exceptionResponse, HttpStatus.valueOf(LoginErrorCode.DuplicateIdFound.getCode()));
     }
 
-
+    /**
+     * Handler :: Forbidden :: 권한 오류 -- 접근불가
+     * @param ex
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(MemberDuplicationException.class)
+    public ResponseEntity<Object> ForbiddenException(MemberDuplicationException ex, WebRequest request) {
+        log.info("Exception Duplication [" + ex.getClass() + "] [" + ex.getMessage() + "]");
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.UNAUTHORIZED);
+    }
 }
--- src/test/java/com/foodPro/demo/ApiTest/MemberApiTest.java
+++ src/test/java/com/foodPro/demo/ApiTest/MemberApiTest.java
@@ -1,37 +1,81 @@
 package com.foodPro.demo.ApiTest;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foodPro.demo.member.domain.Member;
+import com.foodPro.demo.member.dto.MemberDto;
+import com.foodPro.demo.member.repository.MemberRepository;
 import com.foodPro.demo.member.service.MemberService;
+import com.foodPro.demo.member.service.MemberServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@WebMvcTest
 @Transactional
 public class MemberApiTest {
 
     @Autowired
     WebTestClient webTestClient;
 
-    @MockBean @Qualifier("memberService")
-    MemberService memberService;
+    @MockBean
+//    @Qualifier("memberService")
+    MemberRepository memberRepository;
+
+    @LocalServerPort
+    private int port;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
 
 
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+//                .apply(springSecurity())
+                .build();
+    }
 
+    /**
+     * Exception UNIT TEST
+     * 1. get error
+     * @throws Exception
+     */
     @Test
-    public void GlobalException() {
+    public void GlobalException() throws Exception {
         //given
         Long id = 1L;
-        given(memberService.findById(id))
+        given(memberRepository.findById(id))
                 .willReturn(null);
 
         //when
@@ -39,4 +83,24 @@ public class MemberApiTest {
                 .expectStatus().is4xxClientError();
     }
 
+    /**
+     * 2. 이메일 중복
+     * @throws Exception
+     */
+    @Test
+    public void 중복() throws Exception{
+        //given
+        String email = "admin5@gmail.com";
+        Member member = new Member();
+        member.setEmail(email);
+        em.persist(member);
+        em.flush();
+        String url = "http://localhost:" + port + "/api/member/checkEmail";
+        //when
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(MemberDto.Request.builder().email(email).build())))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
 }
